4e399a09a65b4343be350987d7a854b535ee56b0
seeren <consultant@seeren.fr>
2025-10-25 00:31:32 +0200

    feat: Add customer CA

--- src/features/profile/data.ts
+++ src/features/profile/data.ts
@@ -1,6 +1,6 @@
 export const data = [
 
-    'Directeur des opérations digitales, je pilote un programme stratégique pour un client majeur (>100 collaborateurs) avec une équipe de 13 personnes.',
+    'Directeur des opérations digitales, je pilote un programme stratégique pour un client majeur (CA > 40 M€) avec une équipe de 13 personnes.',
     'Je structure les équipes et workflows, définis les priorités et veille à l’engagement des équipes tout en maximisant la valeur business.'
 
 ];
