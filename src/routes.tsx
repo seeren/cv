787915a3decbdb7e403c8ff53265c8ba6fe5bab3
seeren <consultant@seeren.fr>
2026-04-15 00:35:48 +0200

    Refactor CV routes: remove Cv component and update routing to use OneColumn directly.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import OneColumn from "./features/one-column/OneColumn";
-import Detailled from "./features/detailled/Detailled";
-import Cv from "./features/Cv";
 
 export const routes = createBrowserRouter([
-    {
-        path: "/cv",
-        element: <Cv />,
-    },
-    {
-        path: "/cv/one-column",
-        element: <OneColumn />,
-    },
-    {
-        path: "/cv/detailled",
-        element: <Detailled />,
-    },
+  {
+    path: "/cv",
+    element: <OneColumn />,
+  },
 ]);
